9cf1db6978176457ac4a9badcc94849aaa0ad995
Fojan Babaali <fozhanmail@gmail.com>
2021-01-22 18:24:58 -0800

    Update main.js

--- Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/main.js
+++ Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/main.js
@@ -1,22 +1,37 @@
 
 class Bank {
     constructor(AccountNumber, Balance, AccountType, OpeningDate, AccountStatus) {
-        //Properties:
+        //----------------------Properties---------------------
         this.AccountNumber = AccountNumber;
         this.Balance = Balance;
         this.AccountType = AccountType;
         this.OpeningDate = OpeningDate;
         this.AccountStatus = AccountStatus;
-
-        //Methods:
-        Bank.prototype.OpenAccount= function(){
-            if (this.AccountNumber != "") {
-                alert(this.AccountNumber);
-            }}
+        //----------------------Methods---------------------
+        this.OpeningAccount();//Method#1
+        this.CloseAccount();//Method#2
+        this.Credit();//Method#3
         
-            // bank.prototype.CloseAccount = function () { },
+            
             // bank.prototype.Credit = function () { },
             // bank.prototype.Debit = function () { };
+    };//End of the constructor
+        // -----------------------Methods Definition---------------------
+    OpeningAccount(){
+            return this.OpeningDate;
+        };
+    CloseAccount(){
+        if (this.Balance <=1000){
+            return 'Closed Account';}
+            else { return 'Still Active Account';}
+    };
+    Credit(){
+        if (this.Balance>=40000){
+            console.log('Eligible for $20K credits')
+
+        }
 
     }
+
+
 }
