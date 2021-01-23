c78c92e824d840b5b8e2b7be7cdc13df7e8d9aa6
Fojan Babaali <fozhanmail@gmail.com>
2021-01-22 21:51:08 -0800

    Update player.js

--- Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
+++ Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
@@ -23,7 +23,40 @@ class Palyer{
     };//End of constructor
 
         //-------------Methods Definition-----------
-        
+        die(){
+            if ((this.gridLocationY<0 && this.gridLocationX<0)||
+            (this.gridLocationY>9 && this.gridLocationX>9)) {
+                return 'Game Over.Your location is out of th game board.';
+            }
+                else {
+                    if ((0<=gridLocationY<=9) && (0<=gridLocationX<=9)) {
+                        moveLeft();
+                        moveRight();
+                        moveUp();
+                        moveDown();
+                        
+                    }
+                }
+            };
+            moveLeft(){
+                this.gridLocationX-=1;
+                return this.gridLocationX;
+            };
+
+            moveRight(){
+                this.gridLocationX+=1;
+                return this.gridLocationX;   
+            };
+
+            moveUp(){
+                this.gridLocationY+=1;
+                return this.gridLocationY;
+            };
+            moveDown(){
+                this.gridLocationY-=1;
+                return this.gridLocationY;
+            }
+
 
 
 
