5cb904fba046a3b5ff861e3dcfac86380dca57d4
Fojan Babaali <fozhanmail@gmail.com>
2021-01-23 15:41:54 -0800

    Update player.js

--- Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
+++ Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
@@ -1,19 +1,15 @@
 class Palyer{
     constructor(name,livesLeft,score,speed,gridLocationX,gridLocationY){
         //-----------------Properties-------------------
-        // name => (String)
-        // livesLeft => (integer)
-        // score => (integer)
-        // speed => (integer)
-        // gridLocationX => (integer)
-        // gridLocationY => (integer)
-        this.name=name;
-        this.livesLeft=livesLeft;
-        this.score=score;
-        this.speed=speed;
-        this.gridLocationX=gridLocationX;
-        this.gridLocationY=gridLocationY;
+        this.name=name;// name => (String)
+        this.livesLeft=livesLeft;// livesLeft => (integer)
+        this.score=score;// score => (integer)
+        this.speed=speed;// speed => (integer)
+        this.gridLocationX=gridLocationX;// gridLocationX => (integer)
+        this.gridLocationY=gridLocationY;// gridLocationY => (integer)
+
         //-----------------Methods----------------------
+        // this.get();
         this.die();
         this.moveLeft();
         this.moveRight();
@@ -22,42 +18,43 @@ class Palyer{
 
     };//End of constructor
 
-        //-------------Methods Definition-----------
-        die(){
-            if ((this.gridLocationY<0 && this.gridLocationX<0)||
-            (this.gridLocationY>9 && this.gridLocationX>9)) {
-                return 'Game Over.Your location is out of th game board.';
-            }
-                else {
-                    if ((0<=gridLocationY<=9) && (0<=gridLocationX<=9)) {
-                        moveLeft();
-                        moveRight();
-                        moveUp();
-                        moveDown();
-                        
-                    }
-                }
-            };
-            moveLeft(){
+    //-------------Methods Definition-----------
+    // get(_gridLocationX,_gridLocationY){
+        
+    //     this.gridLocationY=prompt('Please enter Y :');
+    //     this.gridLocationX=prompt('Please enter X :');
+    //     return 'Your current location is :'+this.gridLocationX+"'"+this.gridLocationY;
+    // };
+    die(){
+        if ((this.gridLocationY<0 && this.gridLocationX<0)||
+        (this.gridLocationY>9 && this.gridLocationX>9)) {
+            return 'Game Over.Your location is out of th game board.';
+        }
+            
+        if ((0<=gridLocationY<=9) && (0<=gridLocationX<=9)) {
+            moveLeft()
+            {
                 this.gridLocationX-=1;
-                return this.gridLocationX;
-            };
-
-            moveRight(){
-                this.gridLocationX+=1;
-                return this.gridLocationX;   
-            };
-
-            moveUp(){
-                this.gridLocationY+=1;
-                return this.gridLocationY;
-            };
-            moveDown(){
-                this.gridLocationY-=1;
-                return this.gridLocationY;
+                return this.gridLocationX;}
             }
+                    
+                
+        }
+        
+       
 
+        // moveRight(){
+        //     this.gridLocationX+=1;
+        //     return this.gridLocationX;   
+        // };
 
+        // moveUp(){
+        //     this.gridLocationY+=1;
+        //     return this.gridLocationY;
+        // };
+        // moveDown(){
+        //     this.gridLocationY-=1;
+        //     return this.gridLocationY;
+        // }
 
-
-}
+}//End of class
