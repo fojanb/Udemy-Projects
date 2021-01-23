08ca0cec95da742178a107cc0dec39c283369997
Fojan Babaali <fozhanmail@gmail.com>
2021-01-22 18:46:27 -0800

    Update main.js

--- Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/main.js
+++ Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/main.js
@@ -11,27 +11,27 @@ class Bank {
         this.OpeningAccount();//Method#1
         this.CloseAccount();//Method#2
         this.Credit();//Method#3
-        
-            
-            // bank.prototype.Credit = function () { },
-            // bank.prototype.Debit = function () { };
+        this.Debit();//Method#4     
     };//End of the constructor
-        // -----------------------Methods Definition---------------------
+
+        // --------------------Methods Definition-------------
     OpeningAccount(){
             return this.OpeningDate;
-        };
+        };//End of Method#1
     CloseAccount(){
         if (this.Balance <=1000){
             return 'Closed Account';}
-            else { return 'Still Active Account';}
-    };
+            else {return 'Still Active Account';}
+        };//End of Method#2
     Credit(){
         if (this.Balance>=40000){
-            console.log('Eligible for $20K credits')
+            return 'You are eligible for $20K credits';}
+            else {return 'You are NOT eligible for $20K credits'}
+        };//End of Method#3
+    Debit(){
+        if (this.AccountStatus=='Active'){
+            return 'Debit card still in access';}
+            else {return 'Debit card out of access'}
+        }//End of Method#4
 
-        }
-
-    }
-
-
-}
+}//End of the Class
