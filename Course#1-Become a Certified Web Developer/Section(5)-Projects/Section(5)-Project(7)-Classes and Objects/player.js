d0ea1dc70377ce18f53ccf65e3713ff1ee86603a
Fojan Babaali <fozhanmail@gmail.com>
2021-01-23 17:40:28 -0800

    Update player.js

--- Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
+++ Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
@@ -1,4 +1,4 @@
-class Palyer{
+class Player{
     constructor(name,livesLeft,score,speed,gridLocationX,gridLocationY){
         //-----------------Properties-------------------
         this.name=name;// name => (String)
@@ -9,7 +9,7 @@ class Palyer{
         this.gridLocationY=gridLocationY;// gridLocationY => (integer)
 
         //-----------------Methods----------------------
-        // this.get();
+        this.get();//Will return the current location.
         this.die();
         this.moveLeft();
         this.moveRight();
@@ -19,42 +19,84 @@ class Palyer{
     };//End of constructor
 
     //-------------Methods Definition-----------
-    // get(_gridLocationX,_gridLocationY){
+    get(_gridLocationX,_gridLocationY){
         
-    //     this.gridLocationY=prompt('Please enter Y :');
-    //     this.gridLocationX=prompt('Please enter X :');
-    //     return 'Your current location is :'+this.gridLocationX+"'"+this.gridLocationY;
-    // };
-    die(){
+        this.gridLocationY=prompt('Please enter Y :');
+        this.gridLocationX=prompt('Please enter X :');
+        document.getElementById('player-1').innerHTML = 'Your current location is :'+"<br>"+"Y = "+this.gridLocationY+"<br>"+"X = "+this.gridLocationX;
+       
         if ((this.gridLocationY<0 && this.gridLocationX<0)||
-        (this.gridLocationY>9 && this.gridLocationX>9)) {
-            return 'Game Over.Your location is out of th game board.';
-        }
+        (this.gridLocationY>9 && this.gridLocationX>9) || (this.gridLocationY<0 || this.gridLocationX<0) ||(this.gridLocationY>9 || this.gridLocationX>9)) {
+            document.getElementById('player-1-failed').innerHTML= 'Game Over.Your location is out of the game board.';
+        }//End of if
             
-        if ((0<=gridLocationY<=9) && (0<=gridLocationX<=9)) {
-            moveLeft()
-            {
-                this.gridLocationX-=1;
-                return this.gridLocationX;}
-            }
+        if ((0<=this.gridLocationY && this.gridLocationY<=9) && (0<=this.gridLocationX && this.gridLocationX<=9)) {
+            this.move = prompt('Make a move (L/R/U/D) :');
+            switch (this.move) {
+                
+                case "L":
+                case "l":
+                    this.gridLocationX=this.gridLocationX-1;
+                    document.getElementById('moveToLeft').innerHTML= 'You moved to the left. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+                    break;
+                case "R":
+                case "r":   
+                    this.gridLocationX=parseInt(this.gridLocationX)+1;
+                    document.getElementById('moveToRight').innerHTML= 'You moved to the right. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+                    break;
+                case "U":
+                case "u":
+                    this.gridLocationY=parseInt(this.gridLocationY)+1;
+                    document.getElementById('moveToUp').innerHTML= 'You moved to the up. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+                    break;
+                case "D":
+                case "d":
+                    this.gridLocationY=this.gridLocationY-1;
+                    document.getElementById('moveToDown').innerHTML= 'You moved to the up. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+            
+                }//End of switch
+                
+            }//End of if
+                
+            
+          
+    };//End of method
+
+    // moveLeft()
+    //         {
+    //             this.gridLocationX=this.gridLocationX-1;
+    //             document.getElementById('moveToLeft').innerHTML= this.gridLocationX;
+    //         }
+            
+
+    // moveRight(_gridLocationX,_gridLocationY)
+    //         {
+    //             this.gridLocationY+=1;
+    //             document.getElementById('moveToRight').innerHTML= this.gridLocationY;
+    //         }
+    
+
+
+    // die(_gridLocationX,_gridLocationY){
+       
                     
                 
-        }
+    //     }
         
        
 
-        // moveRight(){
-        //     this.gridLocationX+=1;
-        //     return this.gridLocationX;   
-        // };
+    //     moveRight(){
+    //         this.gridLocationX+=1;
+    //         return this.gridLocationX;   
+    //     };
 
-        // moveUp(){
-        //     this.gridLocationY+=1;
-        //     return this.gridLocationY;
-        // };
-        // moveDown(){
-        //     this.gridLocationY-=1;
-        //     return this.gridLocationY;
-        // }
+    //     moveUp(){
+    //         this.gridLocationY+=1;
+    //         return this.gridLocationY;
+    //     };
+    //     moveDown(){
+    //         this.gridLocationY-=1;
+    //         return this.gridLocationY;
+    //     }
 
 }//End of class
