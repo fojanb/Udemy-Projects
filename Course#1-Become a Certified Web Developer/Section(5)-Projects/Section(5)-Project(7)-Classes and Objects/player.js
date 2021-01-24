aae462654a7776330c440cb97911eb05d732ceb9
Fojan Babaali <fozhanmail@gmail.com>
2021-01-23 21:08:09 -0800

    Update player.js

--- Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
+++ Course#1-Become a Certified Web Developer/Section(5)-Projects/Section(5)-Project(7)-Classes and Objects/player.js
@@ -21,38 +21,52 @@ class Player{
     //-------------Methods Definition-----------
     get(_gridLocationX,_gridLocationY){
         
-        this.gridLocationY=prompt('Please enter Y :');
         this.gridLocationX=prompt('Please enter X :');
-        document.getElementById('player-1').innerHTML = 'Your current location is :'+"<br>"+"Y = "+this.gridLocationY+"<br>"+"X = "+this.gridLocationX;
+        this.gridLocationY=prompt('Please enter Y :');
+        
        
         if ((this.gridLocationY<0 && this.gridLocationX<0)||
         (this.gridLocationY>9 && this.gridLocationX>9) || (this.gridLocationY<0 || this.gridLocationX<0) ||(this.gridLocationY>9 || this.gridLocationX>9)) {
             document.getElementById('player-1-failed').innerHTML= 'Game Over.Your location is out of the game board.';
         }//End of if
+
+        else{document.getElementById('player-1').innerHTML = 'Your current location is :'+"<br>"+"X = "+this.gridLocationX+"<br>"+"Y = "+this.gridLocationY;}
             
         if ((0<=this.gridLocationY && this.gridLocationY<=9) && (0<=this.gridLocationX && this.gridLocationX<=9)) {
             this.move = prompt('Make a move (L/R/U/D) :');
             switch (this.move) {
-                
                 case "L":
                 case "l":
                     this.gridLocationX=this.gridLocationX-1;
-                    document.getElementById('moveToLeft').innerHTML= 'You moved to the left. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+                    if(this.gridLocationX<0){
+                        document.getElementById('moveToLeft').innerHTML= 'You are out of game board.';
+                    }
+                    else{document.getElementById('moveToLeft').innerHTML= 'You moved to the left. Your new location is: '+'<br>'+'X= '+this.gridLocationX+"<br>"+'Y= '+this.gridLocationY;}
                     break;
                 case "R":
                 case "r":   
                     this.gridLocationX=parseInt(this.gridLocationX)+1;
-                    document.getElementById('moveToRight').innerHTML= 'You moved to the right. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+                    if(this.gridLocationX>9){
+                        document.getElementById('moveToRight').innerHTML= 'You are out of game board.';
+                    }
+                        else{document.getElementById('moveToRight').innerHTML= 'You moved to the right. Your new location is: '+'<br>'+'X= '+this.gridLocationX+"<br>"+'Y= '+this.gridLocationY;}
                     break;
                 case "U":
                 case "u":
-                    this.gridLocationY=parseInt(this.gridLocationY)+1;
-                    document.getElementById('moveToUp').innerHTML= 'You moved to the up. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+                    this.gridLocationY=this.gridLocationY-1;
+                    if(this.gridLocationY<0){
+                        document.getElementById('moveToUp').innerHTML= 'You are out of game board.';
+                    }
+                        else{document.getElementById('moveToUp').innerHTML= 'You moved to the up. Your new location is: '+'<br>'+'X= '+this.gridLocationX+"<br>"+'Y= '+this.gridLocationY;}
+                   
                     break;
                 case "D":
                 case "d":
-                    this.gridLocationY=this.gridLocationY-1;
-                    document.getElementById('moveToDown').innerHTML= 'You moved to the up. Your new location is: '+'<br>'+'Y= '+this.gridLocationY+"<br>"+'X= '+this.gridLocationX;
+                    this.gridLocationY=parseInt(this.gridLocationY)+1;
+                    if(this.gridLocationY>9){
+                        document.getElementById('moveToDown').innerHTML= 'You are out of game board.';
+                    }
+                    else{document.getElementById('moveToDown').innerHTML= 'You moved to the down. Your new location is: '+'<br>'+'X= '+this.gridLocationX+"<br>"+'Y= '+this.gridLocationY;}
             
                 }//End of switch
                 
