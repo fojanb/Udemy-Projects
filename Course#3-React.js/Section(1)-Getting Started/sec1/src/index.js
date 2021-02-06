c0c9c82d8f91e0b9ed263d2996fe81ca55a6095b
Fojan Babaali <fozhanmail@gmail.com>
2021-02-05 23:53:01 -0800

    Update index.js

--- Course#3-React.js/Section(1)-Getting Started/sec1/src/index.js
+++ Course#3-React.js/Section(1)-Getting Started/sec1/src/index.js
@@ -2,19 +2,11 @@
 //We could delete index.js from src folder and put render() method
 //in App.js instead.
 import React from "react";
-import ReactDOM from "react-dom"; //render() method belongs to ReactDOM
+import ReactDOM from "react-dom"; //ReactDOM.render() method belongs to ReactDOM
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App name="Peter" age="30"/>,document.getElementById("p1"));
+ReactDOM.render(<App/>,document.getElementById("root"));
 
-ReactDOM.render(<App name="Max" age="45"/>,document.getElementById("p2"));
 
-ReactDOM.render(<App name="Kelly" age="50"/>,document.getElementById("p3"));
-
-ReactDOM.render(<App name="Amir" age="30"/>,document.getElementById("p4"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
