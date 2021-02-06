3f40d7b8eed5c2c692ee19a6591ace31493d8e3d
Fojan Babaali <fozhanmail@gmail.com>
2021-02-05 23:52:50 -0800

    Update App.js

--- Course#3-React.js/Section(1)-Getting Started/sec1/src/App.js
+++ Course#3-React.js/Section(1)-Getting Started/sec1/src/App.js
@@ -1,15 +1,37 @@
 //App.js is our react component (Function Component)
+import React, { Component } from "react"; //React.createElement() is from here
+import "./App.css";
+import Person from "./components/Person";
 
-import './App.css';
+//----------------------Class Component :---------------------------
+class App extends Component { //Class Component
+  render() {
+    //Method #1 :
+    return (
+      <div className="app">
+        <h1>Hi, I am a react app</h1>
+        <Person/>
+        <Person/>
+        <Person/>
+        <Person/>
+        <Person/>
 
-function App(props) { //Function Component
-  return (
-    <div className="person">
-      <h3>Name: {props.name}</h3>
-      <p>Age : {props.age}</p>
-    </div>
-    
-  );
+      </div>
+    );
+    //Method#2 :
+    // return React.createElement('div',{className:'app'},React.createElement('h1',null,'I am Fojan'))
+  }
 }
-
 export default App;
+// --------------------Function Component :--------------------------
+// function App(props) { //Function Component
+//   return (
+//     <div className="app">
+//       <h3>Name: {props.name}</h3>
+//       <p>Age : {props.age}</p>
+//     </div>
+
+//   );
+// }
+
+// export default App;
