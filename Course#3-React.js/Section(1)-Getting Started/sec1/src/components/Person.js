8b8102691e8118c90f7ea42395293644c76422f4
Fojan Babaali <fozhanmail@gmail.com>
2021-02-06 19:41:58 -0800

    Update Person.js

--- Course#3-React.js/Section(1)-Getting Started/sec1/src/components/Person.js
+++ Course#3-React.js/Section(1)-Getting Started/sec1/src/components/Person.js
@@ -5,6 +5,7 @@ const person = (props)=> { //Arrow function
   return (
     <div>
       <h3>Hi I am {props.name} and I am {props.age} years old</h3>
+      <p>{props.children}</p>
     </div>
   );
 }
