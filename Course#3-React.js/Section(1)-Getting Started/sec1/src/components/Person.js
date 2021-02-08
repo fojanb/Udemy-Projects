cd130e5931e36596cb642dc41c793776eecaa133
Fojan Babaali <fozhanmail@gmail.com>
2021-02-07 17:18:20 -0800

    Update Person.js

--- Course#3-React.js/Section(1)-Getting Started/sec1/src/components/Person.js
+++ Course#3-React.js/Section(1)-Getting Started/sec1/src/components/Person.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Person.css";
 
 //Function Component:
 const person = (props)=> { //Arrow function
   return (
-    <div>
+    <div className="person">
       <h3 onClick={props.click}>Hi I am {props.name} and I am {props.age} years old</h3>
       <p>{props.children}</p>
       <input type="text" onChange={props.change}/>
