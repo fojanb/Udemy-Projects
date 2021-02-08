24db55a048c77ecdfd2106add2e0f041e6b81397
Fojan Babaali <fozhanmail@gmail.com>
2021-02-07 16:33:11 -0800

    Update index.js

--- Course#3-React.js/Section(1)-Getting Started/sec1/src/index.js
+++ Course#3-React.js/Section(1)-Getting Started/sec1/src/index.js
@@ -1,6 +1,7 @@
 //index.js is respnsible for 'rendering' the App.js component
 //We could delete index.js from src folder and put render() method
 //in App.js instead.
+
 import React from "react";
 import ReactDOM from "react-dom"; //ReactDOM.render() method belongs to ReactDOM
 import App from "./App";
