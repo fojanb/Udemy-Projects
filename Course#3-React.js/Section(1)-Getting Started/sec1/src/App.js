2e6d1838eb0ef0e69c321da00c6fcc7c6ebc0eba
Fojan Babaali <fozhanmail@gmail.com>
2021-02-07 16:33:08 -0800

    Update App.js

--- Course#3-React.js/Section(1)-Getting Started/sec1/src/App.js
+++ Course#3-React.js/Section(1)-Getting Started/sec1/src/App.js
@@ -1,13 +1,13 @@
 //App.js is our react component (Function Component)
-// import React, { Component } from "react"; //React.createElement() is from here
+// import React, { Component } from "react"; //React.createElement() is from here.also setState()
 import "./App.css";
-import React, { useState } from "react";
-import Person from "./components/Person";
-
-//----------------------Function Component :---------------------------
-const app = (props) => {
-  //Function Component
-  const [personState, setPersonState] = useState({
+import React, { Component} from "react"; //We need this line for building a 'class compnonet'
+import Person from "./components/Person";//App.js is Person.js's parent.
+//-------------------------Class Componenet------------------------------------
+class App extends Component {
+  //Class Component
+  state = {
+    //State is an JS object
     persons: [
       //persons is array of objects (JSON format)
       { name: "Mike", age: 20 },
@@ -15,53 +15,123 @@ const app = (props) => {
       { name: "Niel", age: 40 },
     ],
     otherState :'Other type of state'
-  });
-  console.log(personState);
-  // console.log(setPersonState);
-
-  const eventHandler = () => {
-    console.log("You clicked the button!");
+    
+  };
+  eventHandler = (newName) => {
+    console.log('You clicked the button!');
     //DO NOT DO THIS : this.state.persons[0].name="Fojan"
-    setPersonState({
-      //setState() is a method in Component class and App inheret it.
+    this.setState({ //setState() is a method in 'Component' class and App inheret it.
       persons: [
-        { name: "Fojan", age: 20 },
-        { name: "Jenny", age: 32 },
+        { name: newName, age: 20 },//Instead of hard-coding I passed a parameter to eventHandler()
+        { name: "Jenny", age: 40 },
         { name: "Niel", age: 40 },
-      ],
-      otherState :'Other type of state' //or otherState :personState.otherState
-
-
+      ]
     });
   };
 
-  //Method #1 :
-  return (
-    <div className="app">
-      <button onClick={eventHandler}>Swith Name</button>
-      <h1>Hi, I am a react app</h1>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}
-      >
-        My hobby : Chess
-      </Person>
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}
-      />
-      <Person
-        name={personState.persons[2].name}
-        age={personState.persons[2].age}
-      />
-    </div>
-  );
-  //Method#2 :
-  // return React.createElement('div',{className:'app'},React.createElement('h1',null,'I am Fojan'))
-};
-export default app;
+  changeName = (event)=>{
+    this.setState({ //setState() is a method in 'Component' class and App inheret it.
+      persons: [
+        { name: "Fojan", age: 20 },//Instead of hard-coding I passed a parameter to eventHandler()
+        { name: event.target.value, age: 40 },
+        { name: "Niel", age: 40 },
+      ]
+    });
 
-// --------------------Function Component :--------------------------
+  }
+  render() {
+    //Method #1 :
+    return (
+      <div className="app">
+        {/* We used arrow function to pass parameter to our eventHandler function */}
+        <button onClick={()=>this.eventHandler("Fojan")}>Swith Name</button> 
+        <h1>Hi, I am a react app</h1>
+        <Person
+          click={this.eventHandler.bind(this,"Foji!")}//We used bind() for passing 
+          //parameter to a method
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My hobby : Chess
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          change={this.changeName}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    //Method#2 :
+    // return React.createElement('div',{className:'app'},React.createElement('h1',null,'I am Fojan'))
+  }
+}
+export default App;
+
+
+//----------------------Function Component --------------------------------
+// import "./App.css";
+// import React, { useState} from "react"; //We need this line for building a 'function compnonet'
+// import Person from "./components/Person";
+// const app = (props) => {
+//   const [personState, setPersonState] = useState({
+//     persons: [
+//       //persons is array of objects (JSON format)
+//       { name: "Mike", age: 20 },
+//       { name: "Jenny", age: 32 },
+//       { name: "Niel", age: 40 },
+//     ],
+//     otherState :'Other type of state'
+//   });
+//   console.log(personState);
+//   // console.log(setPersonState);
+
+//   const eventHandler = () => {
+//     console.log("You clicked the button!");
+//     //DO NOT DO THIS : this.state.persons[0].name="Fojan"
+//     setPersonState({
+//       //Note :setState() is a method in Component class and App inheret it.
+//       persons: [
+//         { name: "Fojan", age: 20 },
+//         { name: "Jenny", age: 40 },
+//         { name: "Niel", age: 40 },
+//       ],
+//       otherState :'Other type of state' //or otherState :personState.otherState
+
+
+//     });
+//   };
+
+//   //Method #1 :
+//   return (
+//     <div className="app">
+//       <button onClick={eventHandler}>Swith Name</button>
+//       <h1>Hi, I am a react app</h1>
+//       <Person
+//         name={personState.persons[0].name}
+//         age={personState.persons[0].age}
+//       >
+//         My hobby : Chess
+//       </Person>
+//       <Person
+//         name={personState.persons[1].name}
+//         age={personState.persons[1].age}
+//       />
+//       <Person
+//         name={personState.persons[2].name}
+//         age={personState.persons[2].age}
+//       />
+//     </div>
+//   );
+//   //Method#2 :
+//   // return React.createElement('div',{className:'app'},React.createElement('h1',null,'I am Fojan'))
+// };
+// export default app;
+
+// --------------------other notation of Function Component :--------------------------
 // function App(props) { //Function Component
 //   return (
 //     <div className="app">
@@ -72,54 +142,4 @@ export default app;
 //   );
 // }
 
-// export default App;
-//-------------------------Class Componenet------------------------------------
-// class App extends Component {
-//   //Class Component
-//   state = {
-//     //State is an JS object
-//     persons: [
-//       //persons is array of objects (JSON format)
-//       { name: "Mike", age: 20 },
-//       { name: "Jenny", age: 32 },
-//       { name: "Niel", age: 40 },
-//     ],
-//   };
-//   eventHandler = () => {
-//     console.log('You clicked the button!');
-//     //DO NOT DO THIS : this.state.persons[0].name="Fojan"
-//     this.setState({ //setState() is a method in Component class and App inheret it.
-//       persons: [
-//         { name: "Fojan", age: 20 },
-//         { name: "Jenny", age: 32 },
-//         { name: "Niel", age: 40 },
-//       ],
-//     });
-//   };
-//   render() {
-//     //Method #1 :
-//     return (
-//       <div className="app">
-//         <button onClick={this.eventHandler}>Swith Name</button>
-//         <h1>Hi, I am a react app</h1>
-//         <Person
-//           name={this.state.persons[0].name}
-//           age={this.state.persons[0].age}
-//         >
-//           My hobby : Chess
-//         </Person>
-//         <Person
-//           name={this.state.persons[1].name}
-//           age={this.state.persons[1].age}
-//         />
-//         <Person
-//           name={this.state.persons[2].name}
-//           age={this.state.persons[2].age}
-//         />
-//       </div>
-//     );
-//     //Method#2 :
-//     // return React.createElement('div',{className:'app'},React.createElement('h1',null,'I am Fojan'))
-//   }
-// }
 // export default App;
