0f4c6fd3777290fa32a1478f5e0e71d4d4254b68
Fojan Babaali <fozhanmail@gmail.com>
2021-02-08 22:06:14 -0800

    Update UserOutput.js

--- Course#3-React.js/Section(1)-Getting Started/project1/src/components/UserOutput.js
+++ Course#3-React.js/Section(1)-Getting Started/project1/src/components/UserOutput.js
@@ -5,11 +5,13 @@ import './UserOutput.css';
 // =============================================
 
 class UserOutput extends Component {
-  state = {};
+  state = {
+
+  };
   render() {
     return (
       <div className='flexbox' onClick={this.props.click}>
-        <p>
+        <p><strong style={{color:"blue"}}>User Name : {this.props.user}</strong><br/>
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
