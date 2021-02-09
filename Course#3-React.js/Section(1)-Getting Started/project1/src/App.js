5d6974fc04070064f033a0d0660e0b98f2ef1cbe
Fojan Babaali <fozhanmail@gmail.com>
2021-02-08 22:06:11 -0800

    Update App.js

--- Course#3-React.js/Section(1)-Getting Started/project1/src/App.js
+++ Course#3-React.js/Section(1)-Getting Started/project1/src/App.js
@@ -1,34 +1,48 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import UserInput from './components/UserInput';
-import UserOutput from './components/UserOutput';
+import UserInput from "./components/UserInput";
+import UserOutput from "./components/UserOutput";
 
 class App extends Component {
-  state = {//state is an js object 
+  state = {
+    //state is an js object
     userNames: [
       //userNames is array of js objects (JSON format)
-      { name: "mike@gmail.com"},
-      { name: "jenny@shaw.ca"},
-      { name: "carol2020@hotmail.com"},
-      { name: "arieljackson@gmail.com"},
-      { name: "nikkifox@yahoo.com"}
-    ]
-   }
-   clickHandler = () =>{
-     this.setState(state.userNames[0].name);
+      { name: "mike@gmail.com", id: 0 },
+      { name: "jenny@shaw.ca", id: 1 },
+      { name: "carol2020@hotmail.com", id: 2 },
+      { name: "arieljackson@gmail.com", id:3 },
+      { name: "nikkifox@yahoo.com", id: 4 },
+    ],
+  };
+  clickHandler = (newName) => {
+    //State management(updating the state):
+    this.setState({
+      userNames: [
+        //userNames is array of js objects (JSON format)
+        { name: newName + "@gmail.com", id: 0 },
+        { name: "jenny@shaw.ca", id: 1 },
+        { name: "carol2020@hotmail.com", id: 2 },
+        { name: "arieljackson@gmail.com", id: 3 },
+        { name: "nikkifox@yahoo.com", id: 4 },
+      ],
+    });
+  };
 
-   }
-  render() { 
+  render() {
     return (
       <div className="App">
-      <UserInput/>
-      <UserOutput click={this.clickHandler}/>
-      <br/>
-      <UserOutput />
+        <UserInput />
+        <UserOutput
+          // click={this.clickHandler.bind(this, "Fojan")}
+          user={this.state.userNames[0].name}
+        />
+        <br />
+        <UserOutput />
       </div>
     );
   }
 }
- 
+
 export default App;
