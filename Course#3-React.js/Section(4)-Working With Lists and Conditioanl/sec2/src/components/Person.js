8516c5616b49a52e2d17b6d8f564188bbde68b07
Fojan Babaali <fozhanmail@gmail.com>
2021-02-10 20:44:13 -0800

    Update Person.js

--- Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/components/Person.js
+++ Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/components/Person.js
@@ -4,8 +4,8 @@ import "./Person.css";
 //Function Component:
 const person = (props)=> { //Arrow function
   return (
-    <div className="person">
-      <h3 onClick={props.click}>Hi I am {props.name} and I am {props.age} years old</h3>
+    <div className="person" onClick={props.click}>
+      <h3 >Hi I am {props.name} and I am {props.age} years old</h3>
       <p>{props.children}</p>
       <input type="text" onChange={props.change}/>
     </div>
