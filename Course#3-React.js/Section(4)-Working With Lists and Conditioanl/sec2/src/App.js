648e1d2616457880c80882de3b0a20e9948b77fd
Fojan Babaali <fozhanmail@gmail.com>
2021-02-10 20:44:11 -0800

    Update App.js

--- Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/App.js
+++ Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/App.js
@@ -3,6 +3,7 @@
 import "./App.css";
 import React, { Component } from "react"; //We need this line for building a 'class compnonet'
 import Person from "./components/Person"; //App.js is Person.js's parent.
+import person from "./components/Person";
 //-------------------------Class Componenet------------------------------------
 class App extends Component {
   //Class Component
@@ -12,7 +13,7 @@ class App extends Component {
       //persons is array of objects (JSON format)
       { name: "Mike", age: 20 },
       { name: "Jenny", age: 32 },
-      { name: "Niel", age: 40 },
+      { name: "Niel", age: 40 }
     ],
     otherState: "Other type of state",
     showPersons: false,
@@ -25,6 +26,7 @@ class App extends Component {
     border: "none",
     padding: "15px",
     cursor: "pointer",
+    margin: "10px"
   };
   //-----------------Event Handlers <start>---------------------
   buttonHandler = (newName) => {
@@ -39,7 +41,7 @@ class App extends Component {
       ],
     });
   };
-
+  
   changeNameHandler = (event) => {
     this.setState({
       //setState() is a method in 'Component' class and App inheret it.
@@ -55,42 +57,57 @@ class App extends Component {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow }); //showPersons has been updated with new boolean
   };
+
+  personDeleteHandler = (personIndex) =>{
+    const persons= this.state.persons.slice();//Fetch persons(Array of Objects) in state
+    persons.splice(personIndex,1);//This line will remove item from arr
+    this.setState({persons:persons});
+
+  }
+
+  personAddHandler = () =>{
+    const persons= this.state.persons.slice();
+    persons.push({name:"Zwe",age:50});
+    this.setState({persons:persons});
+  }
   //-----------------Event Handlers <finish>---------------------
 
   render() {
-    let allpersons = null;
-    if (this.state.showPersons) {
-      allpersons = (
-        <div>
-          {this.state.persons.map((person) => {
-            return <Person name={person.name} age={person.age} />;
-          })}
-        </div>
-      );
+    let persons = null;
+      {/* Conditional rendering_If statement using Lists : Way #4 ===> the best practice!!!*/}
+      if (this.state.showPersons) {
+        persons=(
+          <div>
+              {this.state.persons.map((person,index) => {
+                return <Person 
+                click={()=>this.personDeleteHandler(index)} name={person.name} age={person.age}></Person>;
+              })}
+            </div>
+        );
+      {/* Conditional rendering_If statement : Way #3 */}
+      // persons = (
+      //   <div>
+      //       <Person
+      //         //We used bind() for passing parameter to our buttonHandler function:
+      //         click={this.buttonHandler.bind(this, "Foji!")}
+      //         // or click = {() => this.buttonHandler("Foji!")}
+      //         name={this.state.persons[0].name}
+      //         age={this.state.persons[0].age}
+      //       >
+      //         My hobby : Chess
+      //       </Person>
+      //       <Person
+      //         name={this.state.persons[1].name}
+      //         age={this.state.persons[1].age}
+      //         change={this.changeNameHandler} //When working with <input/> tag
+      //       />
+      //       <Person
+      //         name={this.state.persons[2].name}
+      //         age={this.state.persons[2].age}
+      //       />
+      //     </div>
+      // );
     }
-//if statement outside of return:
-    // if (this.state.showPersons) {
-    //   <div>
-    //     <Person
-    //       //We used bind() for passing parameter to our buttonHandler function:
-    //       click={this.buttonHandler.bind(this, "Foji!")}
-    //       // or click = {() => this.buttonHandler("Foji!")}
-    //       name={this.state.persons[0].name}
-    //       age={this.state.persons[0].age}
-    //     >
-    //       My hobby : Chess
-    //     </Person>
-    //     <Person
-    //       name={this.state.persons[1].name}
-    //       age={this.state.persons[1].age}
-    //       change={this.changeNameHandler} //When working with <input/> tag
-    //     />
-    //     <Person
-    //       name={this.state.persons[2].name}
-    //       age={this.state.persons[2].age}
-    //     />
-    //   </div>;
-    // }
 
     //Method #1 :
     return (
@@ -98,8 +115,22 @@ class App extends Component {
         <button style={this.styles} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
+        <button style={this.styles} onClick={this.personsAddHandler}>
+          Add Persons
+        </button>
         <h1>Hi, I am a react app</h1>
-        {this.state.showPersons ? ( //IF-ELSE Conditional statement in JSX
+        {persons}
+        {/* Conditional rendering_Ternary Expression : Way #2 */}
+        {/* {this.state.showPersons ? (
+          <div>
+            {this.state.persons.map((person) => {
+              return <Person name={person.name} age={person.age}></Person>;
+            })}
+          </div>
+        ) : null} */}
+
+        {/* Conditional rendering_Ternary Expression  : Way #1 */}
+        {/* {this.state.showPersons ? ( //IF-ELSE Conditional statement in JSX
           <div>
             <Person
               //We used bind() for passing parameter to our buttonHandler function:
@@ -120,7 +151,7 @@ class App extends Component {
               age={this.state.persons[2].age}
             />
           </div>
-        ) : null}
+        ) : null} */}
       </div>
     );
     //Method#2 :
