e1f31d0b1cd7bd38e347dd10784744de6322a223
Fojan Babaali <fozhanmail@gmail.com>
2021-02-11 22:37:17 -0800

    Update Validation.js

--- Course#3-React.js/Section(4)-Working With Lists and Conditioanl/project2/src/components/Validation.js
+++ Course#3-React.js/Section(4)-Working With Lists and Conditioanl/project2/src/components/Validation.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 class Validation extends Component {
+  state = {
+    lenghtInValidation: 0, //initial
+  };
 
-    render() { 
-        return ( 
-            <div>
-                <h3>your paragraph length is {this.props.pLength}</h3>
-            </div>
-
-         );
+  stateUpdate = () => {
+    this.state.lenghtInValidation = this.props.paragraphLength;
+  };
+  compareLength = () => {
+    if (this.state.lenghtInValidation <= 5) {
+      return <h3>Too short</h3>;
+    } else {
+      if (this.state.lenghtInValidation >= 100) {
+        return <h3>Too long</h3>;
+      }
     }
+  };
+
+  render() {
+    return (
+      //All JSX are here
+      <div>
+        {this.stateUpdate()}
+        {this.compareLength()}
+        <h3></h3>
+      </div>
+    );
+  }
 }
- 
+
 export default Validation;
