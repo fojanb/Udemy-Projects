0cb2ddf1737284f1ff8e4891a5201aa710e65d66
Fojan Babaali <fozhanmail@gmail.com>
2021-02-13 23:30:35 -0800

    Update App.js

--- Course#3-React.js/Section(4)-Working With Lists and Conditioanl/project2/src/App.js
+++ Course#3-React.js/Section(4)-Working With Lists and Conditioanl/project2/src/App.js
@@ -1,45 +1,68 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-import "./components/Validation.js";
 import Validation from "./components/Validation";
+import Char from "./components/Char";
 
 class App extends Component {
   state = {
-    words: [], //Array of words ===> 'Paragraph' is stored here
+    words: " ", //string ===> 'Paragraph' is stored here
     lengthInApp: 0, //initial
   };
 
-  // -----------functions and eventListener--------------
-  newWord = (event) => {
+  // -----------functions and eventHandler--------------
+  newWordHandler = (event) => {
+    //This method are updating our state
     const str = event.target.value;
     this.setState({ words: str });
   };
   countLetter = () => {
     let c = 0; //initial
-    for (let index = 1; index <= this.state.words.length; index++) {
+    for (let index = 0; index < this.state.words.length; index++) {
       c = c + 1;
     }
-    this.state.lengthInApp = c;
+    this.state.lengthInApp = c; // State update
     return c;
+  };
+  showLetterinChar = (index) => {
+    return this.state.words.charAt(index);
   };
 
   // -----------------------------------------------------
   render() {
+    let letters = null;
+    if (true) {
+      letters = (
+        <div>
+          {/* change words ='abc' to words=['abc'] via split(''), then you can use map()*/}
+          {/* Since map() only works for array not string */}
+          {this.state.words.split("").map((letter, index) => {
+            return (
+              <Char
+                // click={() => this.letterDeleteHandler(index)} //A list
+                getLetter={this.showLetterinChar(index)}
+              ></Char>
+            );
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <h3>Please type your paragraph down below:</h3>
+        <h3>Please type your paragraph down below</h3>
         <textarea
           type="text"
           rows="8"
-          placeholder="Minimum character is 5 and maximum character is 100"
-          onChange={(event) => this.newWord(event)}
+          placeholder="Maximum character 100"
+          onChange={(event) => this.newWordHandler(event)}
         />
-        {/* or : <textarea type="text" rows= '8' onChange={this.newWord}/> */}
+        {/* or : <textarea type="text" rows= '8' onChange={this.newWordHandler}/> */}
 
         <p>Character count : {this.countLetter()} </p>
 
         <Validation paragraphLength={this.state.lengthInApp} />
+        {letters}
       </div>
     );
   }
