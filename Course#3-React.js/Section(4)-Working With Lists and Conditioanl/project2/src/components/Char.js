c886c831138abb98d734c76912606abbb2b3e3f0
Fojan Babaali <fozhanmail@gmail.com>
2021-02-15 23:09:34 -0800

    Update Char.js

--- Course#3-React.js/Section(4)-Working With Lists and Conditioanl/project2/src/components/Char.js
+++ Course#3-React.js/Section(4)-Working With Lists and Conditioanl/project2/src/components/Char.js
@@ -3,24 +3,22 @@ import ReactDOM from "react-dom";
 // import "./Char.css";
 
 class Char extends Component {
-  // state = {
-  //   letter: this.props.getLetter,
-  // };
   styles = {
-    display: "inlineBlock",
-    width: "100px",
-    position: "relative",
-    left: "634px",
+    display: "inline",
+    width: "60px",
     padding: "16px",
     margin: "16px",
     textAlign: "center",
-    border: "5px solid #2d3047",
+    border: "3px solid #2d3047",
     backgroundColor: "#FF9B71",
   };
-  updateState
 
   render() {
-    return <div style={this.styles} onClick={this.props.click} >{this.props.getLetter}</div>;
+    return (
+      <div style={this.styles} onClick={this.props.click}>
+        {this.props.getLetter}
+      </div>
+    );
   }
 }
 
