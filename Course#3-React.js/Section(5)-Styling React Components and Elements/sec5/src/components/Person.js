086e6bef4192f31ac67d75cd240209c66ba3407b
Fojan Babaali <fozhanmail@gmail.com>
2021-02-17 20:02:52 -0800

    Update Person.js

--- Course#3-React.js/Section(5)-Styling React Components and Elements/sec5/src/components/Person.js
+++ Course#3-React.js/Section(5)-Styling React Components and Elements/sec5/src/components/Person.js
@@ -1,14 +1,35 @@
 import React from "react";
-import "./Person.css";
+// import "./Person.css";
+import styled from "styled-components";
 
 //Function Component:
-const person = (props)=> { //Arrow function
+const person = (props) => {
+  //Arrow function
+  const StyledPerson = styled.div`
+     {
+      //  Regular CSS here :
+      font-family: monospace;
+      border: 0px solid black;
+      border-radius: 10px;
+      box-shadow: 5px 5px lightslategray;
+      background-color: gold;
+      width: 300px;
+      height: 120px;
+      text-align: center;
+      margin: 0 auto;
+      &:hover { /*Pseudo selectors in 'styled-components'*/
+        color: rgb(156, 12, 212);
+    }
+    }
+  `;
   return (
-    <div className="person" >
-      <h3 onClick={props.click}>Hi I am {props.name} and I am {props.age} years old</h3>
+    <StyledPerson>
+      <h3 onClick={props.click}>
+        Hi I am {props.name} and I am {props.age} years old
+      </h3>
       {/* <p>{props.children}</p> */}
-      <input type="text" onChange={props.change} value={props.name}/>
-    </div>
+      <input type="text" onChange={props.change} value={props.name} />
+    </StyledPerson>
   );
-}
+};
 export default person;
