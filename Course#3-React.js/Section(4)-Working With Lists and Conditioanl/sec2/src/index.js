94ddf1ad8ed21bda245f4151b647b3bab5333e3d
Fojan Babaali <fozhanmail@gmail.com>
2021-03-10 21:06:13 -0800

    Update index.js

--- Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/index.js
+++ Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/index.js
@@ -4,7 +4,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom"; //ReactDOM.render() method belongs to ReactDOM
-import App from "./App";
+import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App/>,document.getElementById("root"));
