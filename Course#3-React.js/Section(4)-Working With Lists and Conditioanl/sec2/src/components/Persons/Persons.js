997b75e32d38be63331eb2c414ac76b302e4970c
Fojan Babaali <fozhanmail@gmail.com>
2021-03-11 19:25:57 -0800

    Update Persons.js

--- Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/components/Persons/Persons.js
+++ Course#3-React.js/Section(4)-Working With Lists and Conditioanl/sec2/src/components/Persons/Persons.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Person from './Person/Person';
+import React from "react";
+import Person from "./Person/Person";
 
-const Persons = (props) =>
-    props.persons.map((person, index) => {
+const Persons = (props) => {
+  console.log("[Persons.js] rendering...");
+  // We did loop over props.persons actually:
+  return props.persons.map((person, index) => {
     return (
       <Person
         click={() => props.clicked(index)} //A list
-        change={(event)=>props.changed(event,person.id)}
+        change={(event) => props.changed(event, person.id)}
         name={person.name}
         age={person.age}
         key={person.id} //Key is UNIQUE since id is unique
       ></Person>
-    )});
+    );
+  });
+};
+
 export default Persons;
